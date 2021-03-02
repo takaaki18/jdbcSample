4c93c426fe2fc114c5cd2331cb2bb1c6adbeb05a
takaaki <takaakitanaka18@gmail.com>
2021-03-02 13:33:59 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main {
 			System.out.println(str.getEmployeeName());
 			System.out.println(str.getDepartmentId());
 			System.out.println(str.getPhone());
-			System.out.println("******************");
 			System.out.println("test");
 		}
 	}
